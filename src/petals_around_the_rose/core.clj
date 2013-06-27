1402891983fd5584cdf40936eca3d236d3ae7dae
Lauro Caetano <laurocaetano1@gmail.com>
2013-06-26 23:19:22 -0300

    Sum the dices

--- src/petals_around_the_rose/core.clj
+++ src/petals_around_the_rose/core.clj
@@ -6,3 +6,7 @@
     (for [x (range 5)] 
       (+ 1 (rand-int 5)))))
 
+(defn guess-score
+  [guessed-value dices]
+  "Correct")
+
--- test/petals_around_the_rose/core_test.clj
+++ test/petals_around_the_rose/core_test.clj
@@ -5,3 +5,7 @@
 (deftest rolls-the-dice
   (testing "Returns 5 dices"
     (is (= 5 (count (roll-dices))))))
+
+(deftest sums-five-dices
+  (testing "Sums 5 dices when the algorithm is sum"
+    (is (= "Correct" (guess-score 5 '(1 1 1 1 1))))))
